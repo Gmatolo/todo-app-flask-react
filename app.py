dab387857234f7df476d9b4d589f5c779bcd1f86
Gerald Matolo (MEA) <mulimatolo@gmail.com>
2022-06-11 19:45:23 +0300

    Initialized Sqlite DB with a todo model class

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+from sqlite3 import dbapi2
 from flask import Flask, render_template, url_for
-
+#database adapter
+from flask import SQLAlchemy
 
 
 app = Flask(__name__)
+#Define the path to the database, test.db
+#/// -> indicate a relative path
+#//// -> absolutepath
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+#initializze the dbapi2
+db = SQLAlchemy(app)
+
+#create model class for the db
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed= db.Column(db.Integer, default=0)
 
 @app.route('/')
 def homepage():
