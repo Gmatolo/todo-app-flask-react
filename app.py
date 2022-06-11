08ff4f8e4f2bb36b6f1e7e0d0019f846fc4aa5cb
Gerald Matolo (MEA) <mulimatolo@gmail.com>
2022-06-11 19:59:19 +0300

    Created a repr function that returns a string for every task created

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ class Todo(db.Model):
     completed= db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    #return a string every time we created a new element
+    def __repr__(self):
+        return '<Task %r> % self.id'
+
 @app.route('/')
 def homepage():
     return render_template("index.html")
